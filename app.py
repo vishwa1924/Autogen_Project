bb659732a408376c0c729bcf4b4ae7fd333dbb7a
vishwa <saivishwa.gaddam.com>
2025-04-26 15:03:14 -0700

    secondcommit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from agents import ResearchAgents
+from data_loader import DataLoader
+load_dotenv()
---
+++ data_loader.py
@@ -0,0 +1,7 @@
+import requests
+import xml.etree.ElementTree as ET
+from scholarly import scholarly
+
+
+class DataLoader:
+    pass
